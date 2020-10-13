f1080965570c400a1ae8f826daedefc7166b1b9c
aichaabdelali <aicha.abdelali6@gmail.com>
2020-10-13 12:37:41 +0200

    Checkpoint axios

---
+++ server.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+const { response } = require("express");
+const express = require("express");
+const app = express();
+const port = 4001;
+
+app.listen(port, (err) => {
+  if (err) console.log(err);
+  else console.log(`Server is runing on ${port}`);
+});
+let city_name = "London";
+app.get("/", function (req, res) {
+  axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=80ad75f1b05c83787f4bf3956165deca`
+    )
+    .then((response) => {
+      let wather = {
+        temperature: response.data.main.temp,
+        description: response.data.weather[0].description,
+      };
+      console.log(wather);
+      res.send(
+        "<html><body>" +
+          "<h1>The Wather in " +
+          city_name +
+          "</h1>" +
+          "<h4> temperature: " +
+          wather.temperature +
+          "</h4>" +
+          "<h4> description: " +
+          wather.description +
+          "</h4>" +
+          "</body></html>"
+      );
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
